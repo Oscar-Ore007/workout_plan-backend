db70a04b0dfcedbb5949650cffb6c0f3ab85d894
Oscar Ore <oscar.ore2011@gmail.com>
2021-05-22 18:16:43 -0400

    created Workout controller methods

--- app/controllers/api/v1/workouts_controller.rb
+++ app/controllers/api/v1/workouts_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::WorkoutsController < ApplicationController
+
+    def index 
+        @workouts = Workout.all 
+        render json: @workouts
+    end 
+
+    def create
+        @workout = Workout.new(workout_params)
+        if @workout.save 
+            render json: @workout
+        else
+            render json: {error: 'Error creating workout '}
+        end 
+    end 
+
+    def show 
+        @workout = Workout.find(params[:id])
+        render json: @workout 
+    end 
+
+    def destroy 
+        @workout = Workout.find(params[:id])
+        @workout.destroy 
+    end 
+
+    private 
+
+    def workout_params 
+        params.require(:workout).permit(:name, :description)
+    end 
+
 end
